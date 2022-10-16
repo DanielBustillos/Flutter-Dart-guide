4094e6c71d5b75e1ff1fcf48e1382ea653ff1ce8
Juan Daniel Bustillos Camargo <juandaniel.bucam@gmail.com>
2022-10-16 15:10:31 -0500

    box bubbles

--- 4.Personal_expenses_app/expenses/lib/widgets/transaction_list.dart
+++ 4.Personal_expenses_app/expenses/lib/widgets/transaction_list.dart
@@ -33,47 +33,24 @@ class TransactionList extends StatelessWidget {
             : ListView.builder(
                 itemBuilder: (ctx, index) {
                   return Card(
-                      child: Row(
-                    children: <Widget>[
-                      Container(
-                        margin:
-                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Theme.of(context).primaryColor,
-                            width: 2,
-                          ),
-                        ),
-                        padding: EdgeInsets.all(10),
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(1)}',
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 25,
-                            color: Theme.of(context).primaryColorDark,
-                          ),
+                    elevation: 3,
+                    margin: EdgeInsets.symmetric(vertical: 8,
+                    horizontal: 20),
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: EdgeInsets.all(10),
+                          child: FittedBox(
+                            child: Text('\$${transactions[index].amount}')),
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text(
-                            transactions[index].title,
-                            style: Theme.of(context).textTheme.titleSmall,
-                          ),
-                          Text(
-                            DateFormat.yMMMMd()
-                                .format(transactions[index].date),
-                            style: TextStyle(
-                              //fontWeight: FontWeight.bold,
-                              fontSize: 14,
-                              color: Colors.grey[650],
-                            ),
-                          ),
-                        ],
+                      title: Text(transactions[index].title, 
+                      style: Theme.of(context).textTheme.titleLarge,),
+                      subtitle: Text(DateFormat.yMMMd().format(transactions[index].date,),
+                      style: Theme.of(context).textTheme.titleMedium,)
                       ),
-                    ],
-                  ));
+                  );
                 },
                 itemCount: transactions.length,
               ));
